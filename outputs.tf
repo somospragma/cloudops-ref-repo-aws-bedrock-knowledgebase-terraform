d467833c6577bb171a0a5a9f6a4bc6aee23a762b
agallego <agallego@pragma.com.co>
2025-07-11 11:10:22 -0500

    Change in output name

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,8 @@
 ################ Outputs ##################
 ###########################################
 
-###########################################
-############ Agent Functions #############
-###########################################
-
-output "agents" {
-  description = "Complete information about all agents created"
+output "knowledgebases" {
+  description = "Complete information about all knowledge bases created"
   value = {
     for k, v in var.knowledgebases : k => {
       knowledgebase_arn = aws_bedrockagent_knowledge_base.knowledge_bases[k].arn
